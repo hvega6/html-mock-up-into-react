0998bc2f6d4fcc72f9fabdf9186a4e678e56af03
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-09 20:28:30 -0800

    Update App.jsx

--- html mock up/src/App.jsx
+++ html mock up/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './src/Header'
+import Article from './src/Article'
+import Footer from './src/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Article/>
+      <Footer/>
     </>
   )
 }
