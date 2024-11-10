d14f30e894eaee3ff51f5ac3753604d339568c81
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-09 20:28:34 -0800

    Create Article.jsx

---
+++ html mock up/src/src/Article.jsx
@@ -0,0 +1,24 @@
+export default function Article() {
+  return (
+    <main>
+      <article>
+        <time dateTime="2020-11-12">11/12/20</time>
+        <h3>On the Street in Brooklyn</h3>
+        <img src="image1.jpg" alt="Description of image 1" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <p>
+          <a href="#continue">Continues...</a>
+        </p>
+      </article>
+      <article>
+        <time dateTime="2020-11-11">11/11/20</time>
+        <h3>Vintage in Vogue</h3>
+        <img src="image2.jpg" alt="Description of image 2" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <p>
+          <a href="#continue">Continues...</a>
+        </p>
+      </article>
+    </main>
+  );
+}
