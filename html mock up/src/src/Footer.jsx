1ac74c4b337ab793f9cc2e0805905d9699ed09e6
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-09 20:28:35 -0800

    Create Footer.jsx

---
+++ html mock up/src/src/Footer.jsx
@@ -0,0 +1,37 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <nav aria-label="Footer Navigation" role="navigation">
+          <ul>
+            <li>
+              <a href="#Home">Home</a>
+            </li>
+            <li>
+              <a href="#Women's">Women's</a>
+            </li>
+            <li>
+              <a href="#Men's">Men's</a>
+            </li>
+            <li>
+              <a href="#On the Street">On the Street</a>
+            </li>
+            <li>
+              <a href="#The Catwalk">The Catwalk</a>
+            </li>
+            <li>
+              <a href="#AdWatch">AdWatch</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Tips">Tips</a>
+            </li>
+          </ul>
+        </nav>
+        <p>&copy; 2013 Valet Industries, Inc</p>
+      </footer>
+    </>
+  );
+}
