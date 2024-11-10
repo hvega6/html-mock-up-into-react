285fd2b0477bca287e0ad1f89d7e1b6b72ae2270
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-11-09 20:28:39 -0800

    Create Nav.jsx

---
+++ html mock up/src/src/Nav.jsx
@@ -0,0 +1,28 @@
+export default function Nav() {
+  return (
+    <>
+      <nav aria-label="Main Navigation" role="navigation">
+        <ul>
+          <li>
+            <a href="#Women's">Women's</a>
+          </li>
+          <li>
+            <a href="#Men's">Men's</a>
+          </li>
+          <li>
+            <a href="#On the Street">On the Street</a>
+          </li>
+          <li>
+            <a href="#The Catwalk">The Catwalk</a>
+          </li>
+          <li>
+            <a href="#AdWatch">AdWatch</a>
+          </li>
+          <li>
+            <a href="#About">About</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
